d5248e97fbbfa28475a429c4f9f2e643e80b64a6
ramesh <rameshkrish022000@gmail.com>
2021-06-09 16:00:30 +0300

    Basic implementation of retrofit

---
+++ app/src/main/java/com/example/retrofitdemo/MainData.java
@@ -0,0 +1,21 @@
+package com.example.retrofitdemo;
+
+public class MainData {
+    String Image, name;
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
